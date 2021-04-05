ff3e7e84f8c9da3011fb895d255ba5b50b9fa7c1
Niall Dagg <daggbros@gmail.com>
2021-04-05 19:24:15 +0100

    changed weather names

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -15,15 +15,15 @@ const WeatherData = () => {
   <p className="weather__tagline">Current Weather Information for <span className="weather-data__city">{name}</span></p>
   <div className="weather-data__box">
     <span className="weather-data__property">
-      <p className="weather-data__title">Temperature</p>
+      <p className="weather-data__title">Temperature (°C)</p>
       <p className="weather-data__value">{temp}</p>
     </span>
     <span className="weather-data__property">
-      <p className="weather-data__title">Humidity</p>
+      <p className="weather-data__title">Humidity (g.kg-1)</p>
       <p className="weather-data__value">{humidity}</p>
     </span>
     <span className="weather-data__property">
-      <p className="weather-data__title">Pressure</p>
+      <p className="weather-data__title">Pressure (psf)</p>
       <p className="weather-data__value">{pressure}</p>
     </span>
   </div>
