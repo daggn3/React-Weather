b83cb58181a0192bd67b2ede95513aa12e073f6b
Niall Dagg <daggbros@gmail.com>
2021-04-06 14:15:04 +0100

    yup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
     <div className="footer">
-        <p> A Simple React Weather App Developed by: Niall Dagg    <a rel="noopener noreferrer" target="_blank" href="https://github.com/daggn3"> My Github</a></p>
+        <p> A React Weather App Developed by: Niall Dagg -    <a rel="noopener noreferrer" target="_blank" href="https://github.com/daggn3"> My Github</a></p>
     </div>
 )
  
