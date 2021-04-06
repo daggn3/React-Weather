a01c5c46812af0448541a22b58ad0d41a277f4f6
Niall Dagg <daggbros@gmail.com>
2021-04-06 14:09:35 +0100

    Updates

---
+++ precache-manifest.03c6142544f91dd7a84aed874873ac31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba031f570cb0dfe13fc4ee625623674a",
+    "url": "/React-Weather/index.html"
+  },
+  {
+    "revision": "7b887ab1d64971c02cf3",
+    "url": "/React-Weather/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "8d6ed481abf36eadcfe2",
+    "url": "/React-Weather/static/css/main.312e6a5a.chunk.css"
+  },
+  {
+    "revision": "7b887ab1d64971c02cf3",
+    "url": "/React-Weather/static/js/2.5e47a948.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Weather/static/js/2.5e47a948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6ed481abf36eadcfe2",
+    "url": "/React-Weather/static/js/main.8cdadcc5.chunk.js"
+  },
+  {
+    "revision": "05fa208f1dd5d4e96e82",
+    "url": "/React-Weather/static/js/runtime-main.6bebba97.js"
+  }
+]);
